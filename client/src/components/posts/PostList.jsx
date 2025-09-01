eb2e24a386cd4f426c5bf88106b4403457fa3892
sajinmp <sajinprasadkm@gmail.com>
2025-09-01 17:55:46 +0530

    feat: display post list

--- client/src/components/posts/PostList.jsx
+++ client/src/components/posts/PostList.jsx
@@ -4,15 +4,32 @@ import { API_URL } from '../../constants';
 function PostList() {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      try {
+        const response = await fetch(`${API_URL}/posts.json`);
+        if (response.ok) {
+          const data = await response.json();
+          setPosts(data);
+        } else {
+          throw response;
+        }
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+      }
+    }
+    fetchPosts();
+  }, []);
+
   return (
     <div>
       <h2>Posts</h2>
-      {posts.map(post => {
+      {posts.map(post => (
         <div key={post.id} className="post-container">
           <h3>{post.title}</h3>
           <p>{post.content}</p>
         </div>
-      })}
+      ))}
     </div>
   );
 }
